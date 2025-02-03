4f424c9cc273fcf5f1ce1ced3f3955259c8d743b
opilane <opilane@HP-CZC0359NNX>
2025-02-03 14:48:40 +0200

    ifesle

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("number control");
+
+            int number = 180;
+
+            //kontrollitakse kas arv on suurem 189-st
+            if (number > 189)
+            {
+                //kui  if vastab tingimustele, siis naitab konsoolis seda arvu
+                Console.WriteLine("number is bigger");
+
+                //if ja esle testimine
+                if (number >=190 && number <= 200)
+                {
+                    Console.WriteLine("number is between 190 to 200");
+                }
+                else
+                {
+                    Console.WriteLine("number is bigger than 200");
+                }
+            }
+            else
+            {
+                Console.WriteLine("number is smaller");
+            }
         }
     }
 }
