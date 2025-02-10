4b35897cc968ebb5a4cf14fe18dd35ca72b77fbd
Opilane TTHK <opilane@tthk.ee>
2025-02-10 13:02:12 +0200

    aa

--- ifeslecalculator/ifeslecalculator/Program.cs
+++ ifeslecalculator/ifeslecalculator/Program.cs
@@ -4,7 +4,62 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("insert frist number and press enter: ");
+            //string tuleb konvertida int andmetüübiks
+            float firstNr = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("insert calculation type by number");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            int calculationType = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("insert second number and press enter: ");
+            //string tuleb konvertida int andmetüübiks
+            float secondNr = float.Parse(Console.ReadLine());
+
+            float result = 0;
+
+            if (calculationType == 1)
+            {
+                result = firstNr + secondNr;
+                Console.WriteLine("Answer: ");
+                Console.WriteLine(firstNr + " + " + secondNr + " = " + result);
+            }
+            else
+            if (calculationType == 2)
+            {
+                result = firstNr - secondNr;
+                Console.WriteLine("Answer: ");
+                Console.WriteLine(firstNr + " - " + secondNr + " = " + result);
+            }
+            else
+            if (calculationType == 3)
+            {
+                result = firstNr * secondNr;
+                Console.WriteLine("Answer: ");
+                Console.WriteLine(firstNr + " * " + secondNr + " = " + result);
+            }
+            else
+            if (calculationType == 4)
+            {
+                if (secondNr != 0)
+                {
+                    result = firstNr / secondNr;
+                    Console.WriteLine("Answer: ");
+                    Console.WriteLine(firstNr + " / " + secondNr + " = " + result);
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
         }
     }
 }
